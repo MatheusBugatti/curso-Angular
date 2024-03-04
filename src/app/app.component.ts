3ab3343f302e2fcefcdad9e1a371db3700b73415
matheus <matheus.info@mazatarraf.com.br>
2024-03-04 14:59:39 -0300

    construction(NewComponent)"Alterações no componente"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+//componentes
 import { NewComponent } from './components/new-component/new-component.component';
+import { TemplateBindingComponent } from './components/template-binding/template-binding.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent],
-  styles :[`
-    h1 {
-      color: red;
-    }
-  `],
+  imports: [RouterOutlet, CommonModule, NewComponent, TemplateBindingComponent],
   template: `
-  <h1>Curso de Angular</h1>
   
-  <app-new-component></app-new-component>
+  <h1>Curso de Angular</h1>
+  <app-template-binding></app-template-binding>
+
   `,
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
 }
-//  <router-outlet></router-outlet>
+//  <app-new-component></app-new-component>
+/* <router-outlet></router-outlet> */

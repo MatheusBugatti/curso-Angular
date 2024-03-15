d54b33509a3980473d36c28f0c7455f28900d69c
matheus <matheus.info@mazatarraf.com.br>
2024-03-15 09:25:32 -0300

    feat(pipes)Aprendizado de Pipes

---
+++ src/app/components/pipes/angular-pipes/angular-pipes.component.ts
@@ -0,0 +1,41 @@
+import { AsyncPipe, 
+  CurrencyPipe, 
+  DatePipe, 
+  DecimalPipe, 
+  JsonPipe, 
+  LowerCasePipe, 
+  PercentPipe, 
+  UpperCasePipe, 
+  registerLocaleData } 
+  from '@angular/common';
+import { Component, LOCALE_ID, signal } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
+import { CustomStringPipe } from '../../../pipes/custom-string.pipe';
+
+@Component({
+  selector: 'app-angular-pipes',
+  standalone: true,
+  imports: [DatePipe,
+    UpperCasePipe,
+    LowerCasePipe,
+    JsonPipe,
+    AsyncPipe, 
+    CurrencyPipe, 
+    DecimalPipe, 
+    PercentPipe,
+    CustomStringPipe],
+  templateUrl: './angular-pipes.component.html',
+  styleUrl: './angular-pipes.component.scss',
+
+})
+
+export class AngularPipesComponent {
+  public date = signal(new Date());
+  public json = signal(['name: Matheus Bugatti']);
+
+  public loadingData$: Observable<string[]> = of([
+    'item1',
+    'item2',
+    'item3',
+  ]).pipe(delay(3000));
+}

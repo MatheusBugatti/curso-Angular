eefd220e395feda60daaf05b9d6fe4079e675d45
matheus <matheus.info@mazatarraf.com.br>
2024-03-15 09:27:52 -0300

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,21 +4,43 @@ import { CommonModule } from '@angular/common';
 
 //componentes
 import { NewComponent } from './components/new-component/new-component.component';
-import { TemplateBindingComponent } from './components/template-binding/template-binding.component';
+import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
+import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
+import { SignalsComponentComponent } from './components/signals-component/signals.component.component';
+import { PaiOuMaeComponent } from "./components/comunicacao-entre-components/pai-ou-mae/pai-ou-mae.component";
+import { AngularPipesComponent } from "./components/pipes/angular-pipes/angular-pipes.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule, NewComponent, TemplateBindingComponent],
-  template: `
-  
+    selector: 'app-root',
+    standalone: true,
+    template: `
+
   <h1>Curso de Angular</h1>
-  <app-template-binding></app-template-binding>
+  <app-angular-pipes/>
 
   `,
+    imports: [
+        TemplateDeferrableViewsComponent,
+        TemplateVariablesComponent,
+        RouterOutlet,
+        CommonModule,
+        NewComponent,
+        TemplateBindingComponent,
+        TemplateControlFlowComponent,
+        SignalsComponentComponent,
+        PaiOuMaeComponent,
+        AngularPipesComponent
+    ]
 })
 export class AppComponent {
   title = 'meu-primeiro-projeto-latest';
 }
-//  <app-new-component></app-new-component>
+
+//<app-pai-ou-mae></app-pai-ou-mae
+//   <app-signals-component></app-signals-component>
+// <app-new-component></app-new-component>
+//<app-template-deferrable-views></app-template-deferrable-views>
+// <app-template-control-flow></app-template-control-flow>
 /* <router-outlet></router-outlet> */
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { registerLocaleData } from '@angular/common';
 
 import { routes } from './app.routes';
 
+//LOCALE PT-BR
+import localePt from '@angular/common/locales/pt';
+registerLocaleData(localePt);
+
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers:[
+    provideRouter(routes),
+  { provide: LOCALE_ID, useValue: 'pt-BR' }
+],
 };

729b594548ee6af7fafea23568523634c72b3b6e
matheus <matheus.info@mazatarraf.com.br>
2024-03-25 11:14:19 -0300

    feat(template-driven)Aprendizadp de templates driven

--- src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
+++ src/app/components/forms/template-driven-forms/template-driven-forms.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { JsonPipe, NgFor } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-forms',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './template-driven-forms.component.html',
   styleUrl: './template-driven-forms.component.scss'
 })
 export class TemplateDrivenFormsComponent {
+  public listaComidas = signal<Array<{comida: string, preco: string}>>([
+      {
+        comida: "x-salada", 
+        preco: "R$10",
+      },
+      {
+        comida: "coxinha",
+        preco: "R$5",
+      },
+      { 
+        comida: "x-TUDO", 
+        preco: "R$20",
+      }
+      
+  ])
 
+  public submitForm(form:NgForm){
+    console.log(form.valid);
+
+    if(form.valid){
+      console.log(form.valid);
+      console.log(form.value);
+      
+    }
+    
+  }
 }
